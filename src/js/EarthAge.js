552eddb1944c20615479bc2828551b45c9bcb1c8
TrevorH08 <trevorh8700@gmail.com>
2022-09-11 23:01:09 -0700

    add passing test for jupiter age

--- src/js/EarthAge.js
+++ src/js/EarthAge.js
@@ -6,4 +6,5 @@ export function EarthAge(ageInput) {
   this.MercuryAge = roundTwoDecimal(ageInput * 0.24);
   this.VenusAge = roundTwoDecimal(ageInput * 0.62);
   this.MarsAge = roundTwoDecimal(ageInput * 1.88);
+  this.JupiterAge = roundTwoDecimal(ageInput * 11.86);
 }
