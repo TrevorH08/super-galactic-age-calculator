5ba1b852b5ae279f24c723d0c182d72613978fda
TrevorH08 <trevorh8700@gmail.com>
2022-09-11 21:43:39 -0700

    add constructor parameters to pass first test

--- __tests__/age.test.js
+++ __tests__/age.test.js
@@ -4,6 +4,6 @@ describe ('EarthAge', () => {
   
   test("display the users Age on Earth as 22", () => {
     const userAge = new EarthAge(22);
-    expect(EarthAge(userAge)).toEqual(22);
+    expect(userAge.Age).toEqual(22);
   });
 })
--- src/js/EarthAge.js
+++ src/js/EarthAge.js
@@ -1,3 +1,3 @@
-export function EarthAge() {
-
+export function EarthAge(ageInput) {
+  this.Age = ageInput
 }
